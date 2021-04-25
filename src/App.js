7d9297f4847df0ba146acc01431a00703a935955
sovernut <orenomail4@gmail.com>
2021-04-25 18:30:15 +0700

    use context provider and consumer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './store/auth-context'
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -11,7 +12,7 @@ function App() {
   useEffect(() => {
     const loginInfo = localStorage.getItem(isLoggedInData)
     if (loginInfo === "1") setIsLoggedIn(true)
-  },[])
+  }, [])
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
@@ -26,13 +27,14 @@ function App() {
   };
 
   return (
-    <React.Fragment>
+    <AuthContext.Provider value={{ isLoggedIn: isLoggedIn }}>
       <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
       <main>
         {!isLoggedIn && <Login onLogin={loginHandler} />}
         {isLoggedIn && <Home onLogout={logoutHandler} />}
       </main>
-    </React.Fragment>
+    </AuthContext.Provider>
+
   );
 }
 
